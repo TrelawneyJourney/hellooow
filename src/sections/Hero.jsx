c11de5cb149f06192fca66d18a07c523ba1ade80
TrelawneyJourney <the-trelawney-journey@outlook.com>
2025-08-01 21:45:11 -0300

    adjusting responsive

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,31 +1,31 @@
 import Container from "../components/Container";
 import HeroImg from "../../public/hero.png";
-import HeroMobileImg from "../../public/hero-mobile.webp";
 import Button from "../components/Button";
 
 export default function Hero() {
   return (
     <section className="relative h-[570px] bg-amber-100">
       <Container>
-        <div className="hero py-16 z-10 flex flex-col justify-center lg:flex-row lg:justify-between">
-          <div className="leftSide py-8 flex flex-col items-center gap-12">
+        <div className="hero py-6 lg:py-16 z-10 flex flex-col justify-center lg:flex-row lg:justify-between">
+          {/**Izquierdo container*/}
+          <div className="py-8 flex flex-col items-center lg:items-start lg:pr-[500px] gap-12">
             {/* Hero tags */}
-            <div className="flex flex-col  gap-5">
+            <div className="flex flex-col items-center lg:items-start gap-5 lg:z-30">
               <p className="hidden md:block text-hero uppercase">
                 <span className="block">decorá tu Hogar</span>
                 <span className="block tracking-[7.7px]">con alma felina </span>
               </p>
 
               {/**Tags Mobile */}
-              <p className="hidden max-md:block uppercase text-4xl tracking-[7.7px] leading-12">
+              <p className="hidden max-md:block uppercase text-3xl tracking-[7.7px] leading-12">
                 <span className="block">decorá tu Hogar</span>
                 <span className="block tracking-[10px]">con alma felina </span>
               </p>
 
-              <p className="text-xl font-semibold tracking-[1.5px]">
+              <p className="text-sm lg:text-xl font-semibold tracking-[1.5px]">
                 Comprás con amor, ayudás con el corazón.
               </p>
-              <p className="font-light text-lg w-lg">
+              <p className="text-center lg:text-start font-light text-sm lg:text-lg w-sm lg:w-lg">
                 Cada compra ayuda a un gato rescatado. Hacés que tu casa se vea
                 bien... y su vida también.
               </p>
@@ -36,14 +36,14 @@ export default function Hero() {
             </div>
           </div>
 
-          <div className="rightSide">
-            {/**Img mobile */}
-            {/* <div className="hidden max-md:block">
-              <img src={HeroImg} alt="" />
-            </div> */}
+          <div className="rightSide flex justify-center lg:justify-end">
             {/**Img Desktop */}
-            <div className="max-lg:block absolute lg:right-28 lg:top-2">
-              <img src={HeroImg} alt="" className="lg:py-8" />
+            <div className="max-lg:static lg:absolute lg:right-28 lg:top-2 lg:z-0">
+              <img
+                src={HeroImg}
+                alt="cuadros decorativos"
+                className="lg:py-8"
+              />
             </div>
           </div>
         </div>
